1ee87b3c6a2e2ef908a6f483df2539aecd78c329
annakashkina <anna.kashkina@amsterdam.tech>
2025-11-20 13:38:00 +0300

    rid test of linter warnings

--- shopping_cart_test.py
+++ shopping_cart_test.py
@@ -1,16 +1,18 @@
-import pytest
 import random
 from shopping_cart import CalculateTotal, display_total
+
 
 def test_display_total(capfd):
     display_total(20)
     out, err = capfd.readouterr()
     assert out == "Total price: 20\n"
 
-def TestCalculateTotal_TestEmpty():
+
+def test_calculate_total__empty_cart():
     assert CalculateTotal([]) == 0, 'Incorrect sum on empty cart'
 
-def TestCalculateTotal_TestRandom():
+
+def test_calculate_total__random_card():
     random.seed(0)
     length = random.randint(1, 300)
     cart = []
@@ -18,10 +20,11 @@ def TestCalculateTotal_TestRandom():
     for x in range(length):
         item_price = random.random() * 100
         total_expected += item_price
-        cart.append( {'name': f'Item {x}', 'price': item_price})
+        cart.append({'name': f'Item {x}', 'price': item_price})
     assert abs(CalculateTotal(cart) - total_expected) < 1e-7, f'Incorrect sum on cart {cart}'
 
-def TestCalculateTotal_TestString():
+
+def test_calculate_total__string_cart():
     random.seed(0)
     length = random.randint(1, 300)
     cart = []
@@ -29,5 +32,5 @@ def TestCalculateTotal_TestString():
     for x in range(length):
         item_price = random.random() * 100
         total_expected += item_price
-        cart.append( {'name': f'Item {x}', 'price': str(item_price)})
+        cart.append({'name': f'Item {x}', 'price': str(item_price)})
     assert abs(CalculateTotal(cart) - total_expected) < 1e-7,  f'Incorrect sum on string cart {cart}'
